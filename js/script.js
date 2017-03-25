db475780270e3b97fa6f7aab1b78cf251f305ca6
OOnnnOO <hi@lidong.me>
2017-03-26 00:51:08 +0800

    the basic player function complete +1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// var audio = document.getElementById('audio');
-
 var playlist = [
     {
         "title": "悟",
@@ -21,81 +19,105 @@ var playlist = [
         "artist": "陈小春",
         "filename": "陈小春 - 离不开你.mp3"
     }
-
-
 ];
-var audio, timeout, song, songId;
-songId = 1;
+var audio, timeout, currentTrack = 0;
+var volume = localStorage.volume || 0.5;
+var playlistItem = "";
+for (var i = 0; i < playlist.length; i++) {
+    playlistItem += "<li>" + playlist[i]['title'] + "-" + playlist[i]['artist'] + "</li>";
+
+}
+$("#playlist").html(playlistItem);
 
 
-function loadMusic(songId) {
-    var item = playlist[songId];
-    var newAudio = "<audio src='https://dn-blogme.qbox.me/music/" + item['filename'] + "'></audio>";
-    $('#music-box').append(newAudio);
+function loadMusic(song) {
+    var item = playlist[song];
+    var newAudio = $("<audio>").html("<source src='https://dn-blogme.qbox.me/music/" + item['filename'] + "'>").appendTo('#music-box');
+    // var newAudio = $("<audio>").html("<source src='./music/" + item['filename'] + "'>").appendTo('#music-box');
     audio = $(newAudio)[0];
     $('.music-title').html(item['title']);
     $('.music-artist').html(item['artist']);
     audio.addEventListener('ended', ended, false);
-    play();
+    audio.addEventListener('timeupdate', updateProgress, false);
+    audio.addEventListener('canplay', play, false);
+    $('#downloadLink').attr('href', "./music/" + item['filename']);
+    audio.volume = volume;
+    $('#volume').val(volume);
+    $('#playlist').children().removeClass('active').eq(song).addClass('active');
 }
 
 var play = function () {
     audio.play();
-    $('.playback').addClass('playing');
-    timeout = setInterval(updateProgress, 500);
+    $('#play-btn').css('display', 'none');
+    $('#pause-btn').css('display', 'inline');
 };
 var pause = function () {
     audio.pause();
-    $('.playback').removeClass('playing');
-    clearInterval(timeout);
+    $('#play-btn').css('display', 'inline');
+    $('#pause-btn').css('display', 'none');
 };
-
+// 更新时间，进度条
 var updateProgress = function () {
-    var p = parseInt(audio.currentTime / audio.duration * 100);
-    var m = parseInt((audio.duration - audio.currentTime) / 60);
-    var s = parseInt(audio.duration - audio.currentTime - 60 * m);
-    $('.music-progress').attr('value', p).html(p + "%");
-    $('#leftTime').html(m + ":" + s);
+    var currentProgress = parseInt(audio.currentTime / audio.duration * 100);
+    var m = parseInt(audio.currentTime / 60);
+    var s = parseInt(audio.currentTime % 60);
+    var am = parseInt(audio.duration / 60);
+    var as = parseInt(audio.duration % 60);
+    if (s < 10) {
+        s = '0' + s;
+    }
+    if (as < 10) {
+        as = '0' + as;
+    }
+    $('.music-progress').attr('value', currentProgress).html(currentProgress + "%");
+    $('#leftTime').html(m + ":" + s + " / " + am + ":" + as);
 };
 
 var switchTrack = function (i) {
     if (i < 0) {
-        song = songId = playlist.length - 1;
+        currentTrack = playlist.length - 1;
     } else if (i >= playlist.length) {
-        song = songId = 0;
+        currentTrack = 0;
     } else {
-        song = songId = i;
+        currentTrack = i;
     }
     $('audio').remove();
-    loadMusic(song);
+    loadMusic(currentTrack);
 };
-
 
 var ended = function () {
-    pause();
     audio.currentTime = 0;
-    switchTrack(songId += 1)
+    switchTrack(currentTrack += 1);
 };
 
-loadMusic(songId);
+
+loadMusic(currentTrack);
 
 // click play button
-$('.playback').on('click', function () {
-    if ($(this).hasClass('playing')) {
-        pause();
-    } else {
-        play();
-    }
+$('#play-btn').on('click', function () {
+    play();
+});
+$('#pause-btn').on('click', function () {
+    pause();
 });
 
 // prev
 $('.prev').on('click', function () {
-    switchTrack(songId -= 1)
+    switchTrack(--currentTrack);
 });
 // next
 $('.next').on('click', function () {
-    switchTrack(songId += 1)
+    switchTrack(++currentTrack);
+});
+// 设置声音
+$('#volume').on('input', function () {
+    $('audio')[0].volume = localStorage.volume = volume = $('#volume').val();
+
 });
 
+$('#playlist').children().on('click', function () {
+    switchTrack($(this).index());
+
+});
 
 
